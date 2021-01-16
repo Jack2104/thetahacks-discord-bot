fc1fdbb1ad75bd1dbb4bce3b546acb6b4b668393
Jack Hill <jackhill2104@gmail.com>
2021-01-17 06:32:39 +1030

    Update TODOs

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
-# TODO: add the bot to the sever (from the developer portal) so we can do stuff
-
 import os
 
 import discord
 from dotenv import load_dotenv
 
 # The token is private, so this stops nasty people hijacking our stuff fom our public repo
-# TODO: add the .env to a .gitingnore so that it doesn't show up on source control (would require creating
-# a local copy on everyone's computers though... I can't even bo bothered rn)
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
